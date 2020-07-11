7bb74379295143f9d03cacd24bd32af7b06b00a4
Maksym Karunos <maksymkarunos@Maksyms-MBP.fios-router.home>
2020-07-11 09:54:20 -0400

    added new views and updated url config

--- polls/views.py
+++ polls/views.py
@@ -1,7 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
-
+from . models import Question
 
 def index(request):
-    return HttpResponse("Sosi hui")
+    latest_question_list = Question.objects.order_by("-pub_date")[:5]
+    output = ", ".join([q.question_text for q in latest_question_list])
+    return HttpResponse(output)
+
+
+def detail(request, question_id):
+    return HttpResponse("You are looking at question %s." % question_id)
+
+
+def results(request, question_id):
+    return HttpResponse("You are looking at results %s." % question_id)
+
+
+def vote(request, question_id):
+    return HttpResponse("You are voting on question %s." % question_id)
