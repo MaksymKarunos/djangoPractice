9b88214f3b79816774fe95cdf86eb081f554c702
MaksymKarunos <maksim.karunos@gmail.com>
2020-07-24 20:36:10 -0400

    updates

--- polls/views.py
+++ polls/views.py
@@ -1,13 +1,16 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import Http404, HttpResponse
-
+from django.utils import timezoneex
 from .models import Question
 
 
-def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    context = {'latest_question_list': latest_question_list}
-    return render(request, 'polls/index.html', context)
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by('-pub_date')[:5]
 
 
 def detail(request, question_id):
